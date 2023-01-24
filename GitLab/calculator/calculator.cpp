1f4526ba376e667145a960f280b21726a3966fd4
Sydoruk Rostislav <33869501+k0r7ess@users.noreply.github.com>
2023-01-24 16:56:11 +0200

    Add files via upload

---
+++ GitLab/calculator/calculator.cpp
@@ -0,0 +1,20 @@
+#include "calculator.h"
+
+int Calculator::Add (int a, int b)
+{
+    return a + b;
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+
+
+// Рядок 1
+// Рядок 2
+// Рядок 3
+// Рядок 4
+// Рядок 5
+// Рядок 6
+// Рядок 7
---
+++ GitLab/calculator/calculator.h
@@ -0,0 +1,17 @@
+class Calculator
+{
+    public:
+    
+        static int Add (int, int);
+        static int Mul (double, double);
+
+};
+
+
+// Рядок 1
+// Рядок 2
+// Рядок 3
+// Рядок 4
+// Рядок 5
+// Рядок 6
+// Рядок 7
